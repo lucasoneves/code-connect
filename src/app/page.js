85fa4345526e505f6d584fc4f6cfa2d0293912b9
lucasoneves <casluhc@gmail.com>
2024-05-08 20:27:55 -0300

    Obtendo a primeira página

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,20 @@
 import { CardPost } from "@/components/CardPost";
+import logger from "@/logger";
 
-const post = {
-  id: 1,
-  cover:
-    "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-  title: "Introdução ao React",
-  slug: "introducao-ao-react",
-  body: "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-  markdown:
-    "```javascript  function HelloComponent() {    return <h1>Hello, world!</h1>;  }  ```",
-  author: {
-    id: 101,
-    name: "Ana Beatriz",
-    username: "anabeatriz_dev",
-    avatar:
-      "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png",
-  },
-};
-
-async function getAllPosts() {
-  const response = await fetch('http://localhost:3042/posts');
+async function getAllPosts(page) {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
   if (!response.ok) {
-    console.log("Ops...não conseguimos trazer os dados")
+    logger.error("Ops...não conseguimos trazer os dados");
+    return [];
   }
 
-  return response.json();
+  logger.info("Posts obtidos com sucesso");
+
+  return await response.json();
 }
 
 export default async function Home() {
-  const posts = await getAllPosts();
+  const {data: posts} = await getAllPosts(1);
   return (
     <main>
       <h2>Code Connect</h2>
