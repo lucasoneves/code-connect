740ab706eafc98fd3b0a34ff181562625c9e8176
lucasoneves <casluhc@gmail.com>
2024-05-10 11:51:57 -0300

    Renderizando title do post

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,5 +1,29 @@
-const PostDetail = () => {
-  return <h2>Post detail</h2>
+import logger from "@/logger";
+
+async function getPostBySlug(slug) {
+  const url = `http://localhost:3042/posts?slug=${slug}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    logger.error("Ops...não conseguimos trazer os dados");
+    return {};
+  }
+
+  logger.info("Posts obtidos com sucesso");
+
+  const data = await response.json();
+
+  if (data.length === 0) {
+    return {}
+  }
+  return data[0];
 }
+
+async function PostDetail({params}) {
+  const post = await getPostBySlug(params.slug);
+
+  console.log(post)
+
+  return <h2>{post.title}</h2>;
+};
 
 export default PostDetail;
