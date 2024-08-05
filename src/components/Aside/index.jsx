1f87a0c1e8163c6ac10c0b74f37c306a6f0f1f98
lucasoneves <casluhc@gmail.com>
2024-08-05 12:01:54 -0300

    Migrations para comentários, server action criada

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import styles from "./aside.module.scss";
-import logo from './logo.png';
+import logo from "./logo.png";
 import Image from "next/image";
 
 export const Aside = () => {
   return (
-    <aside className={[styles['aside']]}>
+    <aside className={[styles["aside"]]}>
       {/* <img src="/logo.png" alt="Code Connect" /> */}
-      <Image src={logo} alt="Lodo da Code Connect" width={128} />
+      <Link href="/">
+        <Image src={logo} alt="Lodo da Code Connect" width={128} />
+      </Link>
     </aside>
   );
 };
